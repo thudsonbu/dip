ed404355f399e88e13d49cc7546c2b9e1b42588c
thudsonbu <hudson_thom@bentley.edu>
2020-08-08 12:05:48 -0600

    In place compression version explicit

--- in_place_compression.py
+++ in_place_compression.py
@@ -15,5 +15,37 @@ def compress_in_place(chars):
         left, i = left + 1, i + 1 
     return chars[:left]
 
+def compress_in_place2(chars):
+    location = 0
+    current_char = chars[location]
+    current_char_count = 1
+    accounted_for = 0
+    while accounted_for < len(chars):
+        if location + current_char_count < len(chars) and current_char == chars[location + current_char_count]:
+            current_char_count += 1
+            accounted_for += 1
+        elif current_char_count > 1:
+            chars[location + 1] = current_char_count
+            location += 2
+            for i in range(2,current_char_count):
+                chars.pop(location)
+            if location < len(chars):
+                current_char = chars[location]
+            else:
+                break
+
+            current_char_count = 1
+        else:
+            location += 1
+            accounted_for += 1
+            current_char_count = 1
+            if location < len(chars):
+                current_char = chars[location]
+            else:
+                break
+    return chars
+
+
 
 print(compress_in_place(['a','a','b','c','c','c','c','a','a','a']))
+print(compress_in_place2(['a','a','b','c','c','c','c','a','a','a']))
