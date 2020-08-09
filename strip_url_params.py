779286e121a527bdce4318316cfc788f35427462
thudsonbu <hudson_thom@bentley.edu>
2020-08-09 09:04:41 -0600

    Refactor to seperate get_params and filter_params functions

--- strip_url_params.py
+++ strip_url_params.py
@@ -7,17 +7,10 @@ def strip_url_params(url, *args):
         params_to_strip += param
     start_of_params = url.find("?")
     if start_of_params > -1:
-        param_substring = url[(start_of_params+1)::]
-        param_statements = param_substring.split("&")
-        param_dictionary = {}
-        for statement in param_statements:
-            split_statement = statement.split("=")
-            param_name = split_statement[0]
-            param_value = split_statement[1]
-            if param_name not in params_to_strip:
-                param_dictionary[param_name] = param_value
+        param_statements = get_params(start_of_params,url)
+        param_dictionary = filter_params(param_statements, params_to_strip)
         url_without_params = url[0:start_of_params+1]
-        for param, value in param_dictionary.items():
+        for param, value in sorted(param_dictionary.items()):
             url_without_params += "" + param + "=" + value + "&"
         return url_without_params[0:-1]
     else:
@@ -26,3 +19,25 @@ def strip_url_params(url, *args):
 print(strip_url_params("https://edabit.com?a=1&b=2&a=2"))
 print(strip_url_params("https://edabit.com?a=1&b=2&a=2", ["b"]))
 print(strip_url_params("https://edabit.com", ["b"]))
+
+
+def get_params(params_start, url):
+    param_substring = url[(params_start+1)::]
+    param_statements = param_substring.split("&")
+    return param_statements
+
+print(get_params(18,"https://edabit.com?a=1&b=2&a=2"))
+
+
+def filter_params(param_statements, params_to_strip):
+    param_dictionary = {}
+    for statement in param_statements:
+        split_statement = statement.split("=")
+        param_name = split_statement[0]
+        param_value = split_statement[1]
+        if param_name not in params_to_strip:
+            param_dictionary[param_name] = param_value
+    return param_dictionary
+
+print(filter_params(get_params(18,"https://edabit.com?a=1&b=2&a=2"),"b"))
+
