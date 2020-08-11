81e28391260e92867a77ad22d28e10a6827d4e9b
thudsonbu <hudson_thom@bentley.edu>
2020-08-10 20:27:29 -0600

    Greedy algorithm for determining largest combined number

---
+++ combine_for_highest.py
@@ -0,0 +1,10 @@
+from functools import cmp_to_key
+
+def largestNum(nums):
+    str_nums = [str(n) for n in sorted(nums, key=cmp_to_key(
+        lambda a, b:
+        1 if str(a) + str(b) < str(b) + str(a)
+        else -1))]
+    return "".join(str_nums)
+
+print(largestNum([17, 7, 2, 45, 727,777]))
