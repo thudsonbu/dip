d9aa2ee9c5cb2d3c324842f4dd53674095327e37
thudsonbu <hudson_thom@bentley.edu>
2020-09-01 22:30:51 -0400

    Compare versions improved (without removing zeros)

--- 8.2020/software_version_comp.py
+++ 8.2020/software_version_comp.py
@@ -1,3 +1,38 @@
+# Hi, here's your problem today. This problem was recently asked by Amazon:
+
+# Version numbers are strings that are used to identify unique states of software products. A version number is in the format a.b.c.d. and so on where a, b, etc. are numeric strings separated by dots. These generally represent a hierarchy from major to minor changes. Given two version numbers version1 and version2, conclude which is the latest version number. Your code should do the following:
+# If version1 > version2 return 1.
+# If version1 < version2 return -1.
+# Otherwise return 0.
+
+# Note that the numeric strings such as a, b, c, d, etc. may have leading zeroes, and that the version strings do not start or end with dots. Unspecified level revision numbers default to 0.
+
+# Example:
+# Input: 
+# version1 = "1.0.33"
+# version2 = "1.0.27"
+# Output: 1 
+# #version1 > version2
+
+# Input:
+# version1 = "0.1"
+# version2 = "1.1"
+# Output: -1
+# #version1 < version2
+
+# Input: 
+# version1 = "1.01"
+# version2 = "1.001"
+# Output: 0
+# #ignore leading zeroes, 01 and 001 represent the same number. 
+
+# Input:
+# version1 = "1.0"
+# version2 = "1.0.0"
+# Output: 0
+# #version1 does not have a 3rd level revision number, which
+# defaults to "0"
+
 def compare_versions(v1,v2):
     v1 = [section for section in v1.split(".")]
     v2 = [section for section in v2.split(".")]
@@ -37,7 +72,36 @@ def remove_leading_zeros(str):
             break
     return str
 
+def compare_versions_improved(v1,v2):
+    v1 = [int(section) for section in v1.split(".")]
+    v2 = [int(section) for section in v2.split(".")]
+    count = 0
+    while count < len(v1) and count < len(v2):
+        if v1[count] == v2[count]:
+            count += 1
+            continue
+        elif v1[count] > v2[count]:
+            return 1
+        else:
+            return -1
+    while count < len(v1):
+        if v1[count] == 0:
+            count += 1
+            continue
+        else:
+            return 1
+    while count < len(v2):
+        if v2[count] == 0:
+            count += 1
+            continue
+        else:
+            return -1
+    return 0
+
+
+
 print(remove_leading_zeros("001"))
 print(remove_leading_zeros("1"))
 
-print(compare_versions("1.0.1","1.0.0.0.0.0"))
+print(compare_versions("1.0.12","1.0.02.0.0.0"))
+print(compare_versions_improved("1.1","1"))
