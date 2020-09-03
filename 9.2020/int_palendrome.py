c13c9451553905dea5f975456fa256570f326253
thudsonbu <hudson_thom@bentley.edu>
2020-09-03 13:14:56 -0400

    Check int palendrom no type conversion working

--- 9.2020/int_palendrome.py
+++ 9.2020/int_palendrome.py
@@ -1,6 +1,6 @@
 # Hi, here's your problem today. This problem was recently asked by Twitter:
 
-# Given an integer, check if that integer is a palindrome. For this problem do not convert the integer to a string to check if it is a palindrome.
+# Given an integer, check if that integer is a palindrome.
 
 # import math
 
@@ -13,6 +13,24 @@
 # # False
 
 def check_int_palendrom(num):
+    store = num
+    zeros = 1
+    while num >= zeros:
+        zeros = zeros * 10
+    zeros /= 10
+    new_zeros = 1
+    reversed_num = 0
+    while num > 0:
+        next_digit = int(num/zeros)
+        reversed_num += next_digit*new_zeros
+        num -= next_digit * zeros
+        new_zeros *= 10
+        zeros /= 10
+    return reversed_num - store == 0
+
+    # substract from itself reversed
+
+def check_int_palendrom2(num):
     num = str(num)
     reverse_position = -1
     for position in range(0,int(len(num)/2)):
@@ -27,3 +45,4 @@ print("False " + str(check_int_palendrom(12322)))
 print("True " + str(check_int_palendrom(1)))
 print("True " + str(check_int_palendrom(123321)))
 print("False " + str(check_int_palendrom(12)))
+
