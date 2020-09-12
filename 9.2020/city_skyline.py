ed8263d4e6ad07b2a6d97ed83a694e7c5580447f
thudsonbu <hudson_thom@bentley.edu>
2020-09-12 19:14:05 -0400

    Better solution to skyline problem

--- 9.2020/city_skyline.py
+++ 9.2020/city_skyline.py
@@ -22,6 +22,31 @@ def generate_skyline(buildings):
             current_height = skyline[i]
     return out
 
+import heapq
+
+
+def generate_skyline(buildings):
+    tallest = []
+    skyline = []
+
+    buildings += [(x + 1, x + 1, 0) for (_, x, _) in buildings]
+    buildings.sort(key=lambda x: (x[0], -x[2]))
+
+    for l, r, h in buildings:
+    # Remove shorter buildings behind current building.
+        while tallest and l >= tallest[0][1]:
+            heapq.heappop(tallest)
+
+        # Add current building to heap.
+        heapq.heappush(tallest, (-h, r))
+
+        # Append to skyline if the building is not the tallest.
+        if not skyline or skyline[-1][1] != -tallest[0][0]:
+            skyline.append((l, -tallest[0][0]))
+
+    return skyline
+
+
 #            2 2 2
 #            2 2 2
 #        1 1 2 2 2 1 1
