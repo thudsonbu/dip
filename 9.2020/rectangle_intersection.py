00d8b01c05e85a31e71857a1fd54520e21126d1b
thudsonbu <hudson_thom@bentley.edu>
2020-09-20 11:24:52 -0400

    Optimized solution in 12 minutes

--- 9.2020/rectangle_intersection.py
+++ 9.2020/rectangle_intersection.py
@@ -24,6 +24,11 @@ def intersection_area_helper(values_1,values_2):
     overlap = overlap-1 if overlap > 0 else overlap
     return overlap
 
+def intersection_area2(rect1, rect2):
+    x_intersection = min(rect1.max_x,rect2.max_x) - max(rect1.min_x,rect2.min_x)
+    y_intersection = min(rect1.max_y,rect2.max_y) - max(rect1.min_y,rect2.min_y)
+    return x_intersection*y_intersection if x_intersection > 0 and y_intersection > 0 else 0
+
 #  BBB
 # AXXB
 # AAA
@@ -31,4 +36,6 @@ rect1 = Rectangle(0, 0, 3, 2)
 rect2 = Rectangle(1, 1, 3, 3)
 
 print(intersection_area(rect1, rect2))
+print(intersection_area2(rect1, rect2))
+
 # 2
