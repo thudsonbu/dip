6d9dcea25c9d2e7e2ffeb5af8bc18db53ec810c0
thudsonbu <hudson_thom@bentley.edu>
2020-10-05 22:39:21 -0400

    O(n * k) solution with dynamic programming

--- 9.2020/making_change.py
+++ 9.2020/making_change.py
@@ -17,6 +17,19 @@ def make_change(coins, n):
     else:
         return change_used
 
+def make_change2(coins, n):
+    min_coins = [None] * (n + 1)
+    min_coins[0] = [0]
 
+    for i in range(n):
+        for c in coins:
+            if i + c <= n:
+                if min_coins[i+c] is not None:
+                    min_coins[i+c] = min(min_coins[i] + 1, min_coins[i+c])
+                else:
+                    min_coins[i+c] = min_coins[i] + 1
+
+
+print(make_change([1, 5, 10, 25], 36))
 print(make_change([1, 5, 10, 25], 36))
 # 3 coins (25 + 10 + 1)
