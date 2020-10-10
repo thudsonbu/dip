3d12a13709a414d7497a435adc1cb714acbe3103
thudsonbu <hudson_thom@bentley.edu>
2020-10-09 20:57:29 -0400

    Pos function

--- 10.2020/rotato_array.py
+++ 10.2020/rotato_array.py
@@ -5,7 +5,20 @@
 # Here's an example and some starter code
 
 def rotate_list(nums, k):
-    
+    old_pos = 0
+    old_num = nums[old_pos]
+    new_pos = get_pos(old_pos, len(nums), k)
+    new_num = nums[new_pos]
+    for num in nums:
+        
+
+
+def get_pos(pos, len, k):
+    new_pos = pos + k
+    if new_pos > len:
+        return new_pos - k
+    else:
+        return new_pos
 
 a = [1, 2, 3, 4, 5]
 rotate_list(a, 2)
