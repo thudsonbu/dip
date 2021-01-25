bb65ce6fe964dc339198d71678209462edc0413f
=hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-01-24 21:08:42 -0700

    Make move function and test

--- 1.2021/tower_of_hanoi.js
+++ 1.2021/tower_of_hanoi.js
@@ -2,69 +2,71 @@
 // You cannot move larger disks on top of smaller disks. Create a function that returns
 // the minimum number of steps in order to complete the game.
 
-function towerHanoi( discs ) {
-  let towers = dics % 2 === 0 ? [ [], [ 1 ], [] ] : [ [], [], [ 1 ] ];
+function towerHanoi(discs) {
+  let towers = dics % 2 === 0 ? [[], [1], []] : [[], [], [1]];
 
   let disc = 2;
   let recent_move = null;
   let moves = 1;
 
-  while ( disc <= discs ) {
-    
+  while (disc <= discs) {
     // place in new disc
-    if ( towers[1].length ) {
-      towers[2].push( disc );
+    if (towers[1].length) {
+      towers[2].push(disc);
+      recent_move = 2;
     } else {
-      towers[1].push( disc );
+      towers[1].push(disc);
+      recent_move = 1;
     }
 
     moves += 1;
-    recent_move = disc;
     disc += 1;
 
-    while ( true ) {
+    while (true) {
+      let best_move = findBestMove(towers, recent_move);
 
-      let stuck_stack = getStuckStack( towers, recent_move );
-      let swap = false;
+      towers = makeMove(towers, best_move);
 
-      towers, swap, recent_move = tryOneGreaterSwap( towers, stuck_stack );
+      recent_move = best_move.to;
 
-      if ( !swap ) {
-        
-
-      }
-
+      if (checkStackComplete(towers)) break;
     }
-
-  } 
-
+  }
 }
 
-function findBestMove( towers, stuck_stack ) {
+function makeMove(towers, best_move) {
+  let top = towers[best_move.from].pop();
+  towers[best_move.to].push(top);
+
+  return towers;
+}
+
+let towers = [[3], [2], [5, 4, 1]];
+let best_move = { score: 2, to: 1, from: 2 };
+console.log(makeMove(towers, best_move));
+console.log("should be \n[ [ 3 ], [ 2, 1 ], [ 5, 4 ] ]");
+
+function findBestMove(towers, stuck_stack) {
   let best_move = {
     score: -1,
     to: null,
     from: null,
-  }
+  };
 
-  for ( let i = 0; i < 3; i++ ) {
-    if ( i === stuck_stack || !towers[i].length ) continue;
+  for (let i = 0; i < 3; i++) {
+    if (i === stuck_stack || !towers[i].length) continue;
 
-    for ( let j = 0; j < 3; j++ ) {
-      if ( j === i ) continue;
+    for (let j = 0; j < 3; j++) {
+      if (j === i) continue;
 
       let top = towers[i].slice(-1);
 
       // if its a null place give it a 1
       let bottom;
-      if ( towers[j].length ) {
-       
+      if (towers[j].length) {
         bottom = towers[j].slice(-1);
-
       } else {
-        
-        if ( best_move.score < 1 ) {
-          
+        if (best_move.score < 1) {
           best_move.score = 1;
           best_move.to = j;
           best_move.from = i;
@@ -73,12 +75,11 @@ function findBestMove( towers, stuck_stack ) {
       }
 
       // if its one greater give it a two
-      if ( top == bottom-1 ) {
-
+      if (top == bottom - 1) {
         best_move.score = 2;
         best_move.to = j;
         best_move.from = i;
-      } 
+      }
 
       // garbage moves go here
     }
@@ -87,41 +88,38 @@ function findBestMove( towers, stuck_stack ) {
   return best_move;
 }
 
-let towers = [[3], [2], [5, 4, 1]];
-console.log( findBestMove( towers, 1) );
+towers = [[3], [2], [5, 4, 1]];
+console.log(findBestMove(towers, 1));
 console.log(" should be \n{ score: 2, to: 1, from: 2 }");
 
-towers = [[], [ 2, 1 ], [ 5, 4, 3 ]];
-console.log( findBestMove( towers, 2) );
+towers = [[], [2, 1], [5, 4, 3]];
+console.log(findBestMove(towers, 2));
 console.log(" should be \n{ score: 1, to: 0, from: 1 }");
 
-
-function getStuckStack( towers, recent_move ) {
+function getStuckStack(towers, recent_move) {
   let loc = 0;
   let out;
 
-  towers.forEach( (tower) => {
-
-    if ( tower.slice(-1)[0] === recent_move ) out = loc;
+  towers.forEach((tower) => {
+    if (tower.slice(-1)[0] === recent_move) out = loc;
 
     loc += 1;
-  })
+  });
 
   return out;
 }
 
 towers = [[3], [2], [5, 4, 1]];
-console.log( getStuckStack( towers, 2 ) + " should be 1");
+console.log(getStuckStack(towers, 2) + " should be 1");
 
 towers = [[1], [4, 3, 2], [5]];
-console.log( getStuckStack( towers, 1 ) + " should be 0");
+console.log(getStuckStack(towers, 1) + " should be 0");
 
-function checkStackComplete( towers ) {
+function checkStackComplete(towers) {
   zeros = 0;
 
-  towers.foreach( (tower) => {
-
-    if ( !tower.length ) zeros += 1;
+  towers.foreach((tower) => {
+    if (!tower.length) zeros += 1;
   });
 
   return zeros === 2;
