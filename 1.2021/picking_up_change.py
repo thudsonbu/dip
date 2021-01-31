f1ece468c3bb7eab0f63153f4abc821add0d7e15
thudsonbu <hudson_thom@bentley.edu>
2021-01-30 21:35:13 -0700

    Non recursive solution using iteration for picking up change problem

--- 1.2021/picking_up_change.py
+++ 1.2021/picking_up_change.py
@@ -3,6 +3,32 @@
 # bottom right corner max[n-1][m-1] with the most amount of change. You can only
 # move either left or down.
 
+def max_change(mat):
+  max_change_mat = []
+
+  for i, row in enumerate(mat):
+    max_change_mat.append([0] * len(row))
+
+    for j, cell in enumerate(row):
+
+      if j > 0:
+        max_change_mat[i][j] = max(
+            max_change_mat[i][j - 1] + mat[i][j], max_change_mat[i][j])
+
+      if i > 0:
+        max_change_mat[i][j] = max(
+            max_change_mat[i - 1][j] + mat[i][j], max_change_mat[i][j])
+
+  return max_change_mat[-1][-1]
+
+max = [
+  [0, 3, 0, 2],
+  [1, 2, 3, 3],
+  [6, 0, 3, 2]
+]
+
+assert max_change( mat ) == 13
+
 def traceroute( x, y, mat ):
 
   if ( len( mat[0] ) - 1) > x:
