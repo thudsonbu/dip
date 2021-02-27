5ed6cbd087902851df6791096ec5dc1db25855c1
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-02-27 12:09:40 -0700

    Auto completetion

--- 12.2020/autocompletion.py
+++ 12.2020/autocompletion.py
@@ -1,12 +1,40 @@
 # Implement auto-completion. Given a large set of words (for instance 1,000,000 words) and 
 # then a single word prefix, find all words that it can complete to.
 
+class Node:
+    def __init__(self, isWord, children):
+        self.isWord = isWord # if this is the end of a word we need to know
+        self.children = children
+
 class Solution:
-      def build(self):
-    # Fill this in.
+    def build(self, words):
+        trie = Node(False, {})
+        # traverse tree adding new children if not there
+        for word in words:
+            current = trie
+            for char in word:
+                if not char in current.children:
+                    current.children[char] = Node(False, {})
+                current = current.children[char]
+            current.isWord = True
+        self.trie = trie
     
-  def autocomplete(self, word):
-    # Fill this in.
+    def autocomplete(self, word):
+        current = self.trie
+        for char in word:
+            if not char in current.children:
+                return []
+            current = current.children[char]
+
+        words = []
+        self.dfs(current, word, words)
+        return words
+
+    def dfs(self, node, prefix, words):
+        if node.isWord:
+            words.append(prefix)
+        for char in node.children:
+            self.dfs(node.children[char], prefix + char, words)
 
 s = Solution()
 s.build(['dog', 'dark', 'cat', 'door', 'dodge'])
