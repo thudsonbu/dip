69809ee815d246b33ec3546bb3c2e729b92b1348
thudsonbu <hudson_thom@bentley.edu>
2021-04-02 22:58:50 -0600

    climbing stairs recursive

--- 2021/climbing_stairs.js
+++ 2021/climbing_stairs.js
@@ -10,6 +10,35 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-  
-    
+  let first = helper(0, cost);
+  let second = helper(1, cost);
+
+  if ( first < second ) {
+    return first;
+  } else {
+    return second;
+  }
 };
+
+function helper(index, cost) {
+  let next = 0;
+  let hop = 0;
+
+  if (index+1 < cost.length) {
+    next = helper(index+1, cost);
+  }
+
+  if (index+2 < cost.length) {
+    hop = helper(index+2, cost);
+  }
+
+  if ( next < hop ) {
+    return cost[index] + next;
+  } else {
+    return cost[index] + hop;
+  }
+}
+
+let arr = [1,0,10,11,13]
+
+console.log(minCostClimbingStairs(arr));
