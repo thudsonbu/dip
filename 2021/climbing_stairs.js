8070127f745b24d38608bf6db3a58a945003a02c
thudsonbu <hudson_thom@bentley.edu>
2021-04-02 21:40:46 -0600

    kids candies

---
+++ 2021/climbing_stairs.js
@@ -0,0 +1,15 @@
+// On a staircase, the i-th step has some non-negative cost cost[i] assigned 
+// (0 indexed).
+
+// Once you pay the cost, you can either climb one or two steps. You need to 
+// find minimum cost to reach the top of the floor, and you can either start 
+// from the step with index 0, or the step with index 1.
+
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function(cost) {
+  
+    
+};
