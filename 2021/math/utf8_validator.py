f1d28dc054105ffc0e7fc41e5219cee8f1dd25dc
thudsonbu <hudson_thom@bentley.edu>
2021-04-20 20:47:05 -0600

    utf8 validator

---
+++ 2021/math/utf8_validator.py
@@ -0,0 +1,32 @@
+BYTE_MASKS = [
+    None,
+    0b10000000,
+    0b11100000,
+    0b11110000,
+    0b11111000,
+]
+BYTE_EQUAL = [
+    None,
+    0b00000000,
+    0b11000000,
+    0b11100000,
+    0b11110000,
+]
+
+def utf8_validator(bytes):
+  bytes_len = len(bytes)
+  if bytes_len == 0 or bytes_len > 4:
+    return False
+  if bytes[0] & BYTE_MASKS[bytes_len] != BYTE_EQUAL[bytes_len]:
+    return False
+  for b in bytes[1:]:
+    if b & 0b11000000 != 0b10000000:
+      return False
+  return True
+
+print utf8_validator([0b00000000])
+# True
+print utf8_validator([0b00000000, 10000000])
+# False
+print utf8_validator([0b11000000, 10000000])
+# True
