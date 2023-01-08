e0329beefd43491e8885774f04dc337af800e80e
thudsonbu <54583311+thudsonbu@users.noreply.github.com>
2023-01-07 20:46:54 -0500

    feat: python find duplicate in fs

---
+++ find_dup_in_fs.py
@@ -0,0 +1,42 @@
+# Given a list paths of directory info, including the directory path, and all
+# the files with contents in this directory, return all the duplicate files in
+# the file system in terms of their paths. You may return the answer in any
+# order.
+
+# A group of duplicate files consists of at least two files that have the same
+# content.
+
+# A single directory info string in the input list has the following format:
+
+# "root/d1/d2/.../dm f1.txt(f1_content) f2.txt(f2_content) ...
+# fn.txt(fn_content)" It means there are n files (f1.txt, f2.txt ... fn.txt)
+# with content (f1_content, f2_content ... fn_content) respectively in the
+# directory "root/d1/d2/.../dm". Note that n >= 1 and m >= 0. If m = 0, it means
+# the directory is just the root directory.
+
+# The output is a list of groups of duplicate file paths. For each group, it
+# contains all the file paths of the files that have the same content. A file
+# path is a string that has the following format: "directory_path/file_name.txt"
+
+class Solution:
+    def findDuplicate(self, paths):
+        seen = dict()
+        dups = set()
+
+        for path in paths:
+            self.recordPath(path, dups, seen)
+
+        return [seen[dup] for dup in dups]
+
+    def recordPath(self, path, dups, seen):
+        directory, *files = path.split(' ')
+
+        for file in files:
+            name, content = file.split('(')
+            file_path = directory + '/' + name
+
+            if content in seen:
+                seen[content].append(file_path)
+                dups.add(content)
+            else:
+                seen[content] = [file_path]
