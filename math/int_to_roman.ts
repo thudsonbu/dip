89845f476415b709cbd02b60c7b43a3f0fca4419
Thomas Hudson <54583311+thudsonbu@users.noreply.github.com>
2023-05-22 07:31:13 -0400

    feat: int to roman ts

---
+++ math/int_to_roman.ts
@@ -0,0 +1,29 @@
+const romanSymbolValueMap = [
+  { value: 1000, symbol: "M" },
+  { value: 900, symbol: "CM" },
+  { value: 500, symbol: "D" },
+  { value: 400, symbol: "CD" },
+  { value: 100, symbol: "C" },
+  { value: 90, symbol: "XC" },
+  { value: 50, symbol: "L" },
+  { value: 40, symbol: "XL" },
+  { value: 10, symbol: "X" },
+  { value: 9, symbol: "IX" },
+  { value: 5, symbol: "V" },
+  { value: 4, symbol: "IV" },
+  { value: 1, symbol: "I" },
+];
+
+function intToRoman(num: number): string {
+  let remainder = num;
+  let result = "";
+
+  for (const { value, symbol } of romanSymbolValueMap) {
+    while (remainder >= value) {
+      remainder -= value;
+      result += symbol;
+    }
+  }
+
+  return result;
+}
