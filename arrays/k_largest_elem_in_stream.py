accbeb3b172c1f0ff793bbe3521e5f133062a95b
Thomas Hudson <54583311+thudsonbu@users.noreply.github.com>
2023-05-28 10:33:34 -0400

    feat: kth largest using heap

---
+++ arrays/k_largest_elem_in_stream.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+# Design a class to find the kth largest element in a stream. Note that it is
+# the kth largest element in the sorted order, not the kth distinct element.
+
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
